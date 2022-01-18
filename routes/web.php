a1d1febde3b168124bb6228369a292439785b5e6
Nemuru-san <nimemanta@gmail.com>
2022-01-18 11:00:50 +0700

    admin page success

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Expr\FuncCall;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dasboard');
 });
